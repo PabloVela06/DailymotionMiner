dc3749eea6a349ed8f382cd70b9736c1d0865a5d
Hugo Párraga <hugpardel@alum.us.es>
2026-05-08 19:00:50 +0200

    All prepared

--- src/main/java/aiss/dailymotionminer/DailymotionMinerApplication.java
+++ src/main/java/aiss/dailymotionminer/DailymotionMinerApplication.java
@@ -6,10 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class DailyMotionMinerApplication {
+public class DailymotionMinerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DailyMotionMinerApplication.class, args);
+        SpringApplication.run(DailymotionMinerApplication.class, args);
     }
 
     @Bean
@@ -17,5 +17,4 @@ public class DailyMotionMinerApplication {
         return new RestTemplate();
     }
 
-    public hola es una prueba
 }
