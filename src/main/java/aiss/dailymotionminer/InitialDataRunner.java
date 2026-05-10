e157787439e87c2419df860c35182e92732bb8d4
PabloVela06 <pablovelsan06@gmail.com>
2026-05-10 17:17:52 +0200

    InitialDataRunner done

---
+++ src/main/java/aiss/dailymotionminer/InitialDataRunner.java
@@ -0,0 +1,34 @@
+package aiss.dailymotionminer;
+import aiss.dailymotionminer.repositories.ChannelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InitialDataRunner implements CommandLineRunner {
+
+    @Autowired
+    ChannelRepository c;
+
+    public InitialDataRunner(ChannelRepository c) {
+        this.c = c;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        System.out.println("Arrancando proceso de carga inicial automática...");
+
+        try {
+            Thread.sleep(15000);
+            System.out.println("¡Lanzando la petición a PeerTube y enviando a VideoMiner!");
+            c.create("ivan.cano.gomez-201",10,2,"guille6767");
+            c.create("x1mbetv",10,2,"guille6767");
+
+            System.out.println("¡Carga inicial completada con éxito!");
+
+        } catch (Exception e) {
+            System.out.println("Error en la carga inicial: " + e.getMessage());
+        }
+    }
+}
--- src/main/java/aiss/dailymotionminer/service/AuxiliarFunction.java
+++ src/main/java/aiss/dailymotionminer/service/AuxiliarFunction.java
@@ -2,7 +2,7 @@ package aiss.dailymotionminer.service;
 
 import org.springframework.http.HttpHeaders;
 
-public class AuxiliarFunction {
+public class    AuxiliarFunction {
 
     public static HttpHeaders getApiKeyHeader(String apiKey) {
         HttpHeaders headers = new HttpHeaders();
@@ -11,6 +11,6 @@ public class AuxiliarFunction {
     }
 
     public static String getVideoMinerUri(String path) {
-        return String.format("http://localhost:8080/videominer%s", path);
+        return String.format("https://videominer-production-57ba.up.railway.app/videominer%s", path);
     }
 }
